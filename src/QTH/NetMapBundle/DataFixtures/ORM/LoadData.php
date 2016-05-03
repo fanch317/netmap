d452c3a3cc17ab7c92ab8ba225fe78472d01c5b5
François CARIOU <fanch-github@qth.fr>
2016-05-03 15:49:08 +0200

    Add package doctrine-fixture-bundle and sample fixture

---
+++ src/QTH/NetMapBundle/DataFixtures/ORM/LoadData.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace QTH\NetMapBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+
+class LoadData implements FixtureInterface, ContainerAwareInterface
+{
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+    /**
+     * {@inheritDoc}
+     */
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+    /**
+     * {@inheritDoc}
+     */
+    public function load( ObjectManager $manager )
+    {
+        $manager->flush();
+    }
+}
+
